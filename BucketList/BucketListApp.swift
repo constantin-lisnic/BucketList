1ade0866b6cdade889728c8314444d2db0af944d
Constantin Lisnic <constantin@lisnic.dev>
2024-12-18 09:16:03 +0000

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Constantin Lisnic on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
